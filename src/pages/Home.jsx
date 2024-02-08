3361fc7f1e3b7af4dd65f23fb80c4b386111f558
alves <xodnqjtjt00@gmail.com>
2024-02-08 09:57:22 +0900

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+/* eslint-disable */
+import React, { useState } from 'react'
 
-export default function Home() {
-    return <div>Home</div>;
+export default function TodoComponent() {
+
+    const [todos, setTodos] = useState([
+        {
+          id: 1,
+          text: '리액트 기초 알아보기',
+          checked: true,
+        },
+        {
+          id: 2,
+          text: '컴포넌트 스타일링 하기',
+          checked: true,
+        },
+        {
+          id: 3,
+          text: '투두리스트 만들기',
+          checked: false,
+        },
+      ]);
+
+    const handleTodoInput= ()=>{
+        console.log("input에 있는 내용을 리스트에 넣기");
+    }
+
+    
+  return (
+    <div>
+        <input type="text" />
+        <button onClick={handleTodoInput}>입력</button>
+
+        <ul>
+            {todos.map((item)=>
+                (<li>
+                    <input type="checkbox" name="" id="" checked={item.checked}/>
+                    <span>{item.text}</span>
+                    <button>삭제</button>
+                </li>)
+            )}
+        </ul>
+    </div>
+  )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,62 @@
-import React from "react";
+/* eslint-disable */
+import React, { useState } from "react";
 
 export default function Profile() {
-    return <div>Profile</div>;
+let [TodoList, setTodoList] = useState (["","",""]); //입력값
+let [write, setWrite] = useState (""); //글제목
+let [Success, setSuccess] = useState(0); // 따봉
+ 
+
+
+
+
+    return (
+    
+
+
+    <div>TodoList 작성 
+
+
+<input type='text' className='title' onChange={(e)=> {setTodoList(e.target.value
+    )}}>
+    </input> 
+    <button onClick={()=> {
+if (TodoList == "") {
+    alert("값을 입력해주세요")
+    
+}else{
+let copy  = [...write];
+copy.unshift(TodoList);
+setWrite(copy)
+console.log(write);
+}}}>
+    
+입력
+</button>
+
+
+
+
+
+
+    <Modal  write = {write}></Modal> 
+    </div>
+    )
 }
+
+
+function Modal(props) {
+return(
+<div>
+<h4>{props.write }
+</h4>
+
+</div>
+
+
+
+)
+
+}
+
+
